6acdefc5f0f16eced92d6a3eae8f4c82404c8367
dave8git <dave8git@gmail.com>
2024-10-20 17:20:28 +0200

    Add mooongose

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const mongoClient = require('mongodb').MongoClient;
-
-mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => { // przechowuje informacje dotyczące błędu, client wykorzystamy poniżej
-    if (err) {
-        console.log(err);
-    }
-    else {
-        const db = client.db('companyDB'); // znajdź na serwerze bazę o nazwie companyDB i przypisz jej referencje do stałej db
-        // db.collection('employees')
-        //     .find({ department: 'IT' })
-        //     .toArray()  // musimy dane przekonwertować na zwykłą tablicę, metoda find() zwraca dane w postaci struktury - kursor
-        //     .then((data) => {
-        //         console.log(data);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-        // db.collection('departments')
-        //     .insertOne({ name: 'Management' })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-        // db.collection('employees')
-        //     .updateOne({ department: 'IT'}, { $set: { salary: 6000 }})
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-        // db.collection('departments')
-        //     .deleteOne({ name: 'Management'})
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-
+// const mongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+// mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => { // przechowuje informacje dotyczące błędu, client wykorzystamy poniżej
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+        //const db = client.db('companyDB'); // znajdź na serwerze bazę o nazwie companyDB i przypisz jej referencje do stałej db
         const employeesRoutes = require('./routes/employees.routes');
         const departmentsRoutes = require('./routes/departments.routes');
         const productsRoutes = require('./routes/products.routes');
@@ -41,10 +16,10 @@ mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifi
         app.use(cors());
         app.use(express.json());
         app.use(express.urlencoded({ extended: false }));
-        app.use((req, res, next) => {
-            req.db = db;
-            next(); 
-        });
+        // app.use((req, res, next) => {
+        //     req.db = db;
+        //     next(); 
+        // });
         app.use('/api', employeesRoutes);
         app.use('/api', departmentsRoutes);
         app.use('/api', productsRoutes);
@@ -53,9 +28,17 @@ mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifi
             res.status(404).send({ message: 'Not found...' });
         })
 
+        mongoose.connect('mongodb://0.0.0.0:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = mongoose.connection;
+
+        db.once('open', () => { // once wykryje zdarzenie raz i potem nie ma sensu już na nie nasłuchiwać. ** once - wykrywa zdarzenie tylko raz ** 
+            console.log('Connected to the database');
+        });
+        db.on('error', err => console.log('Error ' + err));
+
         app.listen('8000', () => {
             console.log('Server is running on port: 8000');
         });
         console.log('Succesfully connected to the database');
-    }
-});
+    // }
+// });
